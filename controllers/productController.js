bd98c64ec9bcca560a07e163a593c415a96c836a
Nino <n.popovic@live.com>
2021-02-08 23:59:45 +0100

    fixed comment structure

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,18 @@
 const Product = require('../models/productModel');
-
+/**
+ * function that queries the DB and return products array as data
+ * @param {Object} req -request object
+ * @param {Object} res -response object
+ */
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.json({
+    res.status(200).json({
       status: 'success',
       data: products
     });
   } catch (err) {
     console.log(err);
-    res.send(err);
+    res.status(500).send(err);
   }
 };
